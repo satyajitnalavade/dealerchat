59c82f620b5cdc8e17f019b7303419425704130a
satyajitn <satyajit.nalavade@gmail.com>
2023-04-11 23:29:05 -0500

    Initial commit

---
+++ src/main/java/com/example/dealerchat/DealerchatApplication.java
@@ -0,0 +1,13 @@
+package com.example.dealerchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealerchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealerchatApplication.class, args);
+	}
+
+}
